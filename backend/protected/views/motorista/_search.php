c9d254307f1445059650bd0bb04009c77d79b0da
Lucas Silva <lucassilva@gaudium.com.br>
2025-08-15 11:10:29 -0300

    estiliza página admin de motorista e partial _form

--- backend/protected/views/motorista/_search.php
+++ backend/protected/views/motorista/_search.php
@@ -4,62 +4,78 @@
 /* @var $form CActiveForm */
 ?>
 
-<div class="wide form">
-
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'action'=>Yii::app()->createUrl($this->route),
-	'method'=>'get',
+<?php $form = $this->beginWidget('CActiveForm', array(
+	'action' => Yii::app()->createUrl($this->route),
+	'method' => 'get',
 )); ?>
+<div class="wide form">
+	<section>
+		<div class="row">
+			<div class="col-md-3 mb-4">
+				<p class="mb-2">
+					<?php echo $form->label($model, 'id'); ?>
+				</p>
+				<?php echo $form->textField($model, 'id', array('class' => 'form-control')); ?>
+			</div>
+			<div class="col-md-3 mb-4">
+				<p class="mb-2">
+					<?php echo $form->label($model, 'nome'); ?>
+				</p>
+				<?php echo $form->textField($model, 'nome', array('class' => 'form-control')); ?>
+			</div>
+			<div class="col-md-3 mb-4">
+				<p class="mb-2">
+					<?php echo $form->label($model, 'nascimento'); ?>
+				</p>
+				<?php echo $form->textField($model, 'nascimento', array('class' => 'form-control')); ?>
+			</div>
+			<div class="col-md-3 mb-4">
+				<p class="mb-2">
+					<?php echo $form->label($model, 'email'); ?>
+				</p>
+				<?php echo $form->textField($model, 'email', array('class' => 'form-control')); ?>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-3 mb-4">
+				<p class="mb-2">
+					<?php echo $form->label($model, 'telefone'); ?>
+				</p>
+				<?php echo $form->textField($model, 'telefone', array('class' => 'form-control')); ?>
+			</div>
+			<div class="col-md-3 mb-4">
+				<p class="mb-2">
+					<?php echo $form->label($model, 'placa_veiculo'); ?>
+				</p>
+				<?php echo $form->textField($model, 'placa_veiculo', array('class' => 'form-control')); ?>
+			</div>
+			<div class="col-md-3 mb-4">
+				<p class="mb-2">
+					<?php echo $form->label($model, 'status'); ?>
+				</p>
+				<?php echo $form->textField($model, 'status', array('class' => 'form-control')); ?>
+			</div>
+			<div class="col-md-3 mb-4">
+				<p class="mb-2">
+					<?php echo $form->label($model, 'data_hora_status'); ?>
+				</p>
+				<?php echo $form->textField($model, 'data_hora_status', array('class' => 'form-control')); ?>
+			</div>
+		</div>
 
-	<div class="row">
-		<?php echo $form->label($model,'id'); ?>
-		<?php echo $form->textField($model,'id'); ?>
-	</div>
+		<div class="row">
+			<div class="col-12 mb-4">
+				<p>
+					<?php echo $form->label($model, 'obs'); ?>
+				</p>
+				<?php echo $form->textField($model, 'obs', array('class' => 'w-100 form-control', 'size' => 60, 'maxlength' => 200)); ?>
+			</div>
+		</div>
 
-	<div class="row">
-		<?php echo $form->label($model,'nome'); ?>
-		<?php echo $form->textField($model,'nome',array('size'=>60,'maxlength'=>255)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'nascimento'); ?>
-		<?php echo $form->textField($model,'nascimento'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'email'); ?>
-		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>255)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'telefone'); ?>
-		<?php echo $form->textField($model,'telefone',array('size'=>60,'maxlength'=>255)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'placa_veiculo'); ?>
-		<?php echo $form->textField($model,'placa_veiculo',array('size'=>60,'maxlength'=>255)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'status'); ?>
-		<?php echo $form->textField($model,'status',array('size'=>1,'maxlength'=>1)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'data_hora_status'); ?>
-		<?php echo $form->textField($model,'data_hora_status'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'obs'); ?>
-		<?php echo $form->textField($model,'obs',array('size'=>60,'maxlength'=>200)); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Search'); ?>
-	</div>
-
-<?php $this->endWidget(); ?>
+		<div class="row buttons">
+			<?php echo CHtml::submitButton('Search', array('class' => 'btn btn-outline-secondary')); ?>
+		</div>
+	</section>
+	<?php $this->endWidget(); ?>
 
 </div><!-- search-form -->
--- backend/protected/views/motorista/admin.php
+++ backend/protected/views/motorista/admin.php
@@ -26,33 +26,75 @@ $('.search-form form').submit(function(){
 ");
 ?>
 
-<h1>Motoristas</h1>
+<div class="d-flex flex-column flex-sm-row justify-content-between align-items-center mb-2">
+	<h1>Motoristas</h1>
+	<div>
+		<?php echo CHtml::link('Pesquisa avançada', '#', array('class' => 'btn btn-primary search-button text-white text-decoration-none')); ?>
+		<?php echo CHtml::link('+', array('create'), array('class' => 'btn btn-success')); ?>
+	</div>
+</div>
 
-<?php echo CHtml::link('Advanced Search', '#', array('class' => 'search-button')); ?>
+<!-- search-form -->
 <div class="search-form" style="display:none">
 	<?php $this->renderPartial('_search', array(
 		'model' => $model,
 	)); ?>
-</div><!-- search-form -->
+</div>
 
 <?php $this->widget('zii.widgets.grid.CGridView', array(
 	'id' => 'motorista-grid',
 	'dataProvider' => $model->search(),
 	'filter' => $model,
+	'itemsCssClass' => 'table table-striped table-hover',
 	'columns' => array(
-		'id',
-		'nome',
-		'nascimento',
-		'email',
-		'telefone',
-		'placa_veiculo',
-		/*
-		'status',
-		'data_hora_status',
-		'obs',
-		*/
+		[
+			'name' => 'id',
+			'headerHtmlOptions' => ['class' => 'text-dark text-decoration-none'],
+		],
+		[
+			'name' => 'nome',
+			'headerHtmlOptions' => ['class' => 'text-dark text-decoration-none'],
+		],
+		[
+			'name' => 'nascimento',
+			'headerHtmlOptions' => ['class' => 'text-dark text-decoration-none'],
+		],
+		[
+			'name' => 'email',
+			'headerHtmlOptions' => ['class' => 'text-dark text-decoration-none'],
+		],
+		[
+			'name' => 'telefone',
+			'headerHtmlOptions' => ['class' => 'text-dark text-decoration-none'],
+		],
+		[
+			'name' => 'placa_veiculo',
+			'headerHtmlOptions' => ['class' => 'text-dark text-decoration-none'],
+		],
 		array(
-			'class' => 'CButtonColumn',
+			'header' => 'Ações',
+			'type' => 'raw',
+			'htmlOptions' => array('style' => 'width: 120px; text-align: center;', 'class' => 'text-dark text-decoration-none'),
+			'value' => function ($data) {
+				$urlView = Yii::app()->controller->createUrl('view', array('id' => $data->id));
+				$urlUpdate = Yii::app()->controller->createUrl('update', array('id' => $data->id));
+				$urlDelete = Yii::app()->controller->createUrl('delete', array('id' => $data->id));
+
+				$btnView = "<a href='{$urlView}' class='btn btn-sm btn-outline-info' title='Visualizar'><i class='bi bi-eye-fill'></i></a>";
+				$btnUpdate = "<a href='{$urlUpdate}' class='btn btn-sm btn-outline-primary' title='Editar'><i class='bi bi-pencil-fill'></i></a>";
+
+				$btnDelete = CHtml::link(
+					'<i class="bi bi-trash-fill"></i>',
+					$urlDelete,
+					array(
+						'class' => 'btn btn-sm btn-outline-danger',
+						'title' => 'Deletar',
+						'confirm' => 'Tem certeza que deseja deletar este item?',
+					)
+				);
+
+				return "<div class='d-flex justify-content-center gap-1'>{$btnView} {$btnUpdate} {$btnDelete}</div>";
+			},
 		),
 	),
 )); ?>
