4faae0765594e22c76c8b908c246545e60d587e5
Lucas Silva <lucassilva@gaudium.com.br>
2025-08-15 11:31:09 -0300

    finaliza tarefa 1

--- backend/protected/views/motorista/update.php
+++ backend/protected/views/motorista/update.php
@@ -2,20 +2,13 @@
 /* @var $this MotoristaController */
 /* @var $model Motorista */
 
-$this->breadcrumbs=array(
-	'Motoristas'=>array('index'),
-	$model->id=>array('view','id'=>$model->id),
+$this->breadcrumbs = array(
+	'Motoristas' => array('index'),
+	$model->id => array('view', 'id' => $model->id),
 	'Update',
-);
-
-$this->menu=array(
-	array('label'=>'List Motorista', 'url'=>array('index')),
-	array('label'=>'Create Motorista', 'url'=>array('create')),
-	array('label'=>'View Motorista', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage Motorista', 'url'=>array('admin')),
 );
 ?>
 
-<h1>Update Motorista <?php echo $model->id; ?></h1>
+<h1>Atualizar dados</h1>
 
-<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+<?php $this->renderPartial('_form', array('model' => $model)); ?>
--- backend/protected/views/passageiro/update.php
+++ backend/protected/views/passageiro/update.php
@@ -2,20 +2,13 @@
 /* @var $this PassageiroController */
 /* @var $model Passageiro */
 
-$this->breadcrumbs=array(
-	'Passageiros'=>array('index'),
-	$model->id=>array('view','id'=>$model->id),
+$this->breadcrumbs = array(
+	'Passageiros' => array('index'),
+	$model->id => array('view', 'id' => $model->id),
 	'Update',
-);
-
-$this->menu=array(
-	array('label'=>'List Passageiro', 'url'=>array('index')),
-	array('label'=>'Create Passageiro', 'url'=>array('create')),
-	array('label'=>'View Passageiro', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage Passageiro', 'url'=>array('admin')),
 );
 ?>
 
-<h1>Update Passageiro <?php echo $model->id; ?></h1>
+<h1>Atualizar dados</h1>
 
-<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+<?php $this->renderPartial('_form', array('model' => $model)); ?>
